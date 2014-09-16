4c74bd25d9f9369fb327cfdf29883e9c4e7926cb
Alex Hughes <alex.hughes000@gmail.com>
2014-09-16 15:49:44 -0400

    Added HTML minifier

--- app/config/app.php
+++ app/config/app.php
@@ -123,6 +123,7 @@ return array(
 		'Illuminate\Workbench\WorkbenchServiceProvider',
         'Barryvdh\Debugbar\ServiceProvider',
         'Cartalyst\Sentry\SentryServiceProvider',
+        'Fitztrev\LaravelHtmlMinify\LaravelHtmlMinifyServiceProvider'
 
 	),
 
