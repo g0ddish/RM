aabb69a47e96163e3c5005ae6abd585d2315f48f
Alex Hughes <alex.hughes000@gmail.com>
2014-09-25 17:25:26 -0400

    Index Potential Layout v1

--- app/views/main/index.blade.php
+++ app/views/main/index.blade.php
@@ -1,4 +1,42 @@
-<div class="col-md-12" style="height: 100%; background-color: #55AA55;">
-
+<div class="col-md-12" style="height: 100%; background-color: #55AA55; padding-top:60px;">
+   {{ isset($name) ? $name : "<div class='alert alert-warning'>You haven't filled out all of your profile yet. <a href='./profile'>Do that here.</a></div>" }}
+   <div class="col-md-9">
+      <div class="panel panel-default">
+         <div class="panel-body">
+            <div class="list-group">
+               <a href="#" class="list-group-item active">
+                  <h4 class="list-group-item-heading">List group item heading</h4>
+                  <p class="list-group-item-text">...</p>
+               </a>
+               <a href="#" class="list-group-item">
+                  <h4 class="list-group-item-heading">List group item heading</h4>
+                  <p class="list-group-item-text">...</p>
+               </a>
+               <a href="#" class="list-group-item">
+                  <h4 class="list-group-item-heading">List group item heading</h4>
+                  <p class="list-group-item-text">...</p>
+               </a>
+            </div>
+         </div>
+      </div>
+   </div>
+   <div class="col-md-3">
+      <div class="panel panel-success">
+         <div class="panel-heading">
+            <h3 class="panel-title">Project Tags</h3>
+         </div>
+         <div class="panel-body">
+            <div class="list-group">
+               <a href="#" class="list-group-item active">
+               PHP
+               </a>
+               <a href="#" class="list-group-item">C#</a>
+               <a href="#" class="list-group-item">Balsamiq</a>
+               <a href="#" class="list-group-item">AutoCAD</a>
+               <a href="#" class="list-group-item">SQL</a>
+               <a href="#" class="list-group-item">More...</a>
+            </div>
+         </div>
+      </div>
+   </div>
 </div>
-
