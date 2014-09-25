5cf267605ed2126399cf732e3ff83f771201ea51
Alex Hughes <alex.hughes000@gmail.com>
2014-09-25 16:56:23 -0400

    Branding and navbar stuff

--- app/views/layouts/masterUser.blade.php
+++ app/views/layouts/masterUser.blade.php
@@ -17,6 +17,8 @@
 }
 .navbar-custom .navbar-brand {
   color: #ffffff;
+  padding-top:5px;
+
 }
 .navbar-custom .navbar-brand:hover,
 .navbar-custom .navbar-brand:focus {
@@ -128,14 +130,14 @@
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
       </button>
-      <a class="navbar-brand" href="#">RM</a>
+      <a class="navbar-brand" href="#"><img class="img-responsive" src="{{asset('img/monsters/mon2.png')}}"/></a>
     </div>
-
+    <?php $user = Sentry::getUser(); ?>
     <!-- Collect the nav links, forms, and other content for toggling -->
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
       <ul class="nav navbar-nav">
-        <li class="active"><a href="#">Link</a></li>
-        <li><a href="#">Link</a></li>
+        <li class="active"><a href="./">Home</a></li>
+        <li><a href="./projects">Projects</a></li>
         <li class="dropdown">
           <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <span class="caret"></span></a>
           <ul class="dropdown-menu" role="menu">
@@ -150,6 +152,7 @@
         </li>
       </ul>
       <ul class="nav navbar-nav navbar-right">
+       <li><a href="./profile/<?php echo $user->student_id;?>"><?php echo $user->student_id;?></a></li>
        <li><a href="./logout">Logout</a></li>
       </ul>
 
